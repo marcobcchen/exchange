5439636c499949a21ad5896956afe3c40d28bd39
marcochen <marco8854144@gmail.com>
2021-03-22 18:14:11 +0800

    fix

--- js/features1.js
+++ js/features1.js
@@ -2,6 +2,7 @@ $(function(){
   var ww;
   var contentId_2 = 0;
   var contentId_4 = 0;
+  var timer;
 
   function init(){
     $(window).on('resize', onResize);
@@ -20,6 +21,8 @@ $(function(){
       $('.lb-container').fadeOut();
       return false;
     });
+
+    timer =setInterval(slide, 10);
   }
   init();
 
@@ -32,10 +35,75 @@ $(function(){
 
     var _w = $('.text-img-1 .text').innerWidth();
 
-    TweenMax.fromTo($('.text-img-1 .text'), 1, {x: 0}, {x: 170, ease: Power0.easeNone, repeat: -1, yoyo: true, delay: 0.1});
-    TweenMax.fromTo($('.text-img-3 .text'), 1, {x: 0}, {x: 170, ease: Power0.easeNone, repeat: -1, yoyo: true, delay: 1});
-    TweenMax.fromTo($('.text-img-2 .text'), 1, {x: 170}, {x: 0, ease: Power0.easeNone, repeat: -1, yoyo: true, delay: 0.2});
-    TweenMax.fromTo($('.text-img-4 .text'), 1, {x: 170}, {x: 0, ease: Power0.easeNone, repeat: -1, yoyo: true, delay: 2});
+    // TweenMax.fromTo($('.text-img-1 .text'), 3, {x: 0}, {x: -ww, ease: Power0.easeNone, repeat: -1, delay: 0.1});
+    // TweenMax.fromTo($('.text-img-3 .text'), 1, {x: 0}, {x: 170, ease: Power0.easeNone, repeat: -1, yoyo: true, delay: 1});
+    // TweenMax.fromTo($('.text-img-2 .text'), 1, {x: 170}, {x: 0, ease: Power0.easeNone, repeat: -1, yoyo: true, delay: 0.2});
+    // TweenMax.fromTo($('.text-img-4 .text'), 1, {x: 170}, {x: 0, ease: Power0.easeNone, repeat: -1, yoyo: true, delay: 2});
+  }
+
+  var space = 0;
+
+  function slide(){
+    space = 2;
+    // console.log(0-space);
+    // $('.text-img-1 .text-1').css('left', text_1_1 1);
+    // $('.text-img-1 .text-2').css('left', text_1_2 - 1);
+    
+    var text_1_1 = parseInt($('.text-img-1 .text-1').css('left'));
+    var text_1_2 = parseInt($('.text-img-1 .text-2').css('left'));
+    var text_3_1 = parseInt($('.text-img-3 .text-1').css('left'));
+    var text_3_2 = parseInt($('.text-img-3 .text-2').css('left'));
+    var text_2_1 = parseInt($('.text-img-2 .text-1').css('left'));
+    var text_2_2 = parseInt($('.text-img-2 .text-2').css('left'));
+    var text_4_1 = parseInt($('.text-img-4 .text-1').css('left'));
+    var text_4_2 = parseInt($('.text-img-4 .text-2').css('left'));
+
+    if(text_1_1 < -2164){
+      $('.text-img-1 .text-1').css('left', 2163);
+    } else{
+      TweenMax.set($('.text-img-1 .text-1'), {left: text_1_1 - space});
+    }
+    if(text_1_2 < -2164){
+      $('.text-img-1 .text-2').css('left', 2163);
+    } else{
+      TweenMax.set($('.text-img-1 .text-2'), {left: text_1_2 - space});
+    }
+
+    if(text_3_1 < -2164){
+      $('.text-img-3 .text-1').css('left', 2163);
+    } else{
+      TweenMax.set($('.text-img-3 .text-1'), {left: text_3_1 - space});
+    }
+    if(text_3_2 < -2164){
+      $('.text-img-3 .text-2').css('left', 2163);
+    } else{
+      TweenMax.set($('.text-img-3 .text-2'), {left: text_3_2 - space});
+    }
+
+    if(text_2_1 > 2164){
+      $('.text-img-2 .text-1').css('left', -2163);
+    } else{
+      TweenMax.set($('.text-img-2 .text-1'), {left: text_2_1 + space});
+    }
+    if(text_2_2 > 2164){
+      $('.text-img-2 .text-2').css('left', -2163);
+    } else{
+      TweenMax.set($('.text-img-2 .text-2'), {left: text_2_2 + space});
+    }
+
+    if(text_4_1 > 2164){
+      $('.text-img-4 .text-1').css('left', -2163);
+    } else{
+      TweenMax.set($('.text-img-4 .text-1'), {left: text_4_1 + space});
+    }
+    if(text_4_2 > 2164){
+      $('.text-img-4 .text-2').css('left', -2163);
+    } else{
+      TweenMax.set($('.text-img-4 .text-2'), {left: text_4_2 + space});
+    }
+
+    
+    
   }
 
   function content2(){
